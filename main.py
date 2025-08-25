30ae08f2e2d1842e3786f09d2f83b7e87137d140
skywalker-03 <daniel.moreira08@aluno.ifce.edu.br>
2025-08-25 22:43:35 +0000

    mudanças

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI(title='API de Nicollas e Daniel')
 @app.get("/")
 def helloWorld():
     return{"message":"Hello World!"}
+
+@app.get("/aluno/{nome_aluno}")
+def get_aluno_by_name(nome_aluno: str):
+    return {f"Olá, {nome_aluno}, seja muito bem-vindo(a) ao nosso app!"}
